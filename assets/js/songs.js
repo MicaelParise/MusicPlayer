46b99cca7f847205e09fd0bdb21960aabaf64127
MicaelChaves <88152528+MicaelParise@users.noreply.github.com>
2026-07-01 15:45:55 -0300

    Atualizando readme

--- assets/js/songs.js
+++ assets/js/songs.js
@@ -57,85 +57,71 @@ let allMusic = [
   {
     name: "Yoko",
     artist: "Yoko",
-    img: "music-8",
     src: "music-8"
   },
   {
     name: "SALES",
     artist: "renee",
-    img: "music-9",
     src: "music-9"
   },
   {
     name: "Valasy",
     artist: "Nürnberg",
-    img: "music-10",
     src: "music-10"
   },
   {
     name: "Mondo",
     artist: "Jimmy Fontana",
-    img: "music-11",
     src: "music-11"
   },
   {
     name: "Something About You",
     artist: "Eydress & Dent May",
-    img: "music-12",
     src: "music-12"
   },
   {
     name: "The Outfield",
     artist: "Your Love",
-    img: "music-13",
     src: "music-13"
   },
   {
     name: "Where My Rosemary Goes",
     artist: "Love Grows",
-    img: "music-14",
     src: "music-14"
   },
   {
     name: "Breezeblocks",
     artist: "Alt-J",
-    img: "music-15",
     src: "music-15"
   },
   {
     name: "Home",
     artist: "Edward Sharpe & The Magnetic Zeros",
-    img: "music-16",
     src: "music-16"
   },
   {
     name: "Serasa do amor",
     artist: "Julio Secchin",
-    img: "music-17",
     src: "music-17"
   },
   {
     name: "High Hopes",
     artist: "Kodaline",
-    img: "music-18",
     src: "music-18"
   },
   {
     name: "Outra vida",
     artist: "Armandinho",
-    img: "music-19",
     src: "music-19"
   },
   {
     name: "Freaks",
     artist: "Surf Curse",
-    img: "music-20",
     src: "music-20"
   },
   {
     name: "There Is A Light That Never Goes Out",
     artist: "The Smiths",
-    img: "music-21",
     src: "music-21"
   },
 ];
